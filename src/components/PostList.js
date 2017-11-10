92a559a98fc04e830a28c866c2a331dd6c69f3a1
Jeremy Breidenbach <jeremy.breidenbach@gmail.com>
2017-11-09 22:31:10 -0600

    Infinite scroll

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, FlatList, ScrollView, View } from 'react-native';
+import { ActivityIndicator, FlatList, View } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import axios from 'axios';
 
 class PostList extends Component {
-    state = { isLoading: true, posts: [] };
+    state = {
+        isLoading: false,
+        isRefreshing: false,
+        posts: [],
+        lastPostId: '',
+        page: 1,
+        error: null
+    };
 
-    componentWillMount() {
-        axios.get('https://www.reddit.com/r/aww/top/.json?raw_json=1').then(response => this.setState({ isLoading: false, posts: response.data.data.children }));
-        // axios.get('https://www.reddit.com/r/aww/top/.json?raw_json=1').then(response => console.log(response.data.data.children));
+    componentDidMount() {
+        this.fetchPosts();
     }
+
+
+    fetchPosts = () => {
+        const page = this.state.page;
+        const url = 'https://www.reddit.com/r/aww/top/.json?raw_json=1&count=25' + (page !== 1 ? this.state.lastPostId : '');
+        this.setState({ isLoading: true });
+
+        axios.get(url)
+            .then(response => this.setState({
+                isLoading: false,
+                isRefreshing: false,
+                posts: page ===1 ? response.data.data.children : [...this.state.posts, ...response.data.data.children],
+                lastPostId: response.data.data.after }))
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    fetchMorePosts = () => {
+        this.setState({
+            page: this.state.page + 1,
+        }, () => {
+            this.fetchPosts();
+        });
+    };
 
     renderRow(rowData) {
         return (
@@ -23,33 +54,23 @@ class PostList extends Component {
     render() {
         if (this.state.isLoading) {
             return (
-                <View style={{ flex: 1, paddingTop: 20}}>
+                <View style={{ flex: 1, paddingTop: 20 }}>
                     <ActivityIndicator />
                 </View>
             );
         }
 
         return (
-            //<ScrollView>
                 <List style={styles}>
                     <FlatList
                         data={this.state.posts}
                         renderItem={
                             ({ item }) => <ListItem title={item.data.title} avatar={{ uri: item.data.thumbnail }} />}
                         keyExtractor={(item, index) => index}
+                        onEndReached={this.fetchMorePosts}
+                        onEndThreshold={0.5}
                     />
                 </List>
-                    // {
-                    //     this.state.posts.map((post, i) => (
-                    //         <ListItem
-                    //             key={i}
-                    //             title={post.data.title}
-                    //             avatar={{ uri: post.data.thumbnail }}
-                    //         />
-                    //     ))
-                    // }
-                //</List>
-            //</ScrollView>
         );
     }
 }
